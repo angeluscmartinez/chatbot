a309df3f7b874fd937eed95698d189a3ed0fb5e5
angeluscmartinez <111898653+angeluscmartinez@users.noreply.github.com>
2025-03-14 06:59:38 -0700

    Update Home-26.py

--- Home-26.py
+++ Home-26.py
@@ -92,18 +92,18 @@ st.markdown(
 
 col1, col2, col3 = st.columns([1, 3, 1.5], gap='medium')
 with col1:
-    st.markdown("<div style='display: flex; align-items: center; justify-content: center; height: 180px;'>", unsafe_allow_html=True)
+    st.markdown("<div style='display: flex; align-items: center; justify-content: center; height: auto; align-items: flex-end;'>", unsafe_allow_html=True)
     st.image("Picture1.png", width=316)
     st.markdown("</div>", unsafe_allow_html=True)
     st.markdown("<div style='height: 92px;'></div>", unsafe_allow_html=True)
 with col2:
     st.markdown("""
-    <div style="text-align: center; display: flex; align-items: center; justify-content: center; height: 180px;">
+    <div style="text-align: center; display: flex; align-items: center; justify-content: center; height: auto; align-items: flex-end;">
         <h1 style="margin: auto;">I'm Your Helpful iRIS AI Agent, Ask Me Anything</h1>
     </div>
     """, unsafe_allow_html=True)
 with col3:
-    st.markdown("<div style='display: flex; align-items: center; justify-content: center; height: 180px;'>", unsafe_allow_html=True)
+    st.markdown("<div style='display: flex; align-items: center; justify-content: center; height: auto; align-items: flex-end;'>", unsafe_allow_html=True)
     try:
         lottie_coding = load_lottiefile("coding.json")
     except FileNotFoundError:
@@ -166,6 +166,7 @@ if st.session_state.suggested_questions:
             st.session_state.messages.insert(0, {"role": "assistant", "content": response_text})
             st.session_state.suggested_questions = new_suggestions
             st.rerun()
+
 
 
 
